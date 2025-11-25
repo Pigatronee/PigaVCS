be896f6687c463bac0f65f1ce3da604d56a674f3
PigObsessedGameDev <jamesjimmybob96@gmail.com>
2025-11-25 14:06:49 +0000

    trying to make pip work lol

--- vcs/setup.py
+++ vcs/setup.py
@@ -3,13 +3,14 @@ from setuptools import setup
 setup(
     name="PigaVCS",
     version="0.2",
-    py_modules=["main"],
+    py_modules=["vcs"],
     install_requires=[
         "argparse",
         "datetime",
     ],
-    entry_points='''
-        [console_scripts]
-        pigavcs=main:cli 
-    ''',
+    entry_points={
+        "console_scripts":[
+            "pigavcs = vcs:cli"
+        ]
+    }
 )
